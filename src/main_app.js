e9c0907f20f58044b44a1e480cd6b79018bdde81
HK <hiteshk4373@gmail.com>
2024-10-25 23:07:51 +0530

    PORT update

--- src/main_app.js
+++ src/main_app.js
@@ -38,23 +38,18 @@ const PORT = process.env.TODO_APP_PORT || 3000;
 //   .then(() => app.listen(PORT, () => console.log(`Server running: ${PORT}`)))
 //   .catch((error) => console.log(error));
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port: ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 
-//   // Then connect to MongoDB
-//   mongoose.set("strictQuery", false);
-//   mongoose
-//     .connect(CONN_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     .then(() => console.log("Connected to MongoDB"))
-//     .catch((error) => console.error("Failed to connect to MongoDB:", error));
-// });
-
-mongoose
-  .connect(CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("Failed to connect to MongoDB:", error));
+  // Then connect to MongoDB
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(CONN_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.error("Failed to connect to MongoDB:", error));
+});
 
 export default app;
